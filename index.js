9e7f545e09d33ae2a69d2f5f4dae63d70766a30c
a-h-arman-hussain <armanhd16@gmail.com>
2025-11-10 18:34:56 +0600

    server created add some api created

---
+++ index.js
@@ -0,0 +1,85 @@
+const express = require("express");
+const cors = require("cors");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+// require("dotenv").config();
+const app = express();
+const port = 4000;
+// process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
+
+app.use(cors());
+app.use(express.json());
+
+const uri = `mongodb+srv://online-learning-platform:epbVPU2f2IEPSTJh@cluster0.xrhyseu.mongodb.net/?appName=Cluster0`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+async function run() {
+  try {
+    await client.connect();
+    const db = client.db("course-db");
+    const coursesCollection = db.collection("courses");
+    // all course
+    app.get("/courses", async (req, res) => {
+      const result = await coursesCollection.find().toArray();
+      res.send(result);
+    });
+
+    // my course
+    app.get("/my-course", async (req, res) => {
+      const email = req.query.email;
+      const result = await coursesCollection
+        .find({ created_by: email })
+        .toArray();
+      res.send(result);
+    });
+
+    // latest 6 course
+    app.get("/latest-course", async (req, res) => {
+      const result = await coursesCollection
+        .find()
+        .sort({ created_ad: "desc" })
+        .limit(6)
+        .toArray();
+      console.log(result);
+      res.send(result);
+    });
+
+    // course detail
+    app.get("/courses/:id", async (req, res) => {
+      const { id } = req.params;
+      const result = await coursesCollection.findOne({ _id: new ObjectId(id) });
+      res.send({
+        success: true,
+        result,
+      });
+    });
+
+    app.post("/courses", async (req, res) => {
+      const data = req.body;
+      const result = await coursesCollection.insertOne(data);
+      res.send({
+        success: true,
+        result,
+      });
+    });
+
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+app.listen(port, () => {
+  console.log(`Server is listening on port ${port}`);
+});
